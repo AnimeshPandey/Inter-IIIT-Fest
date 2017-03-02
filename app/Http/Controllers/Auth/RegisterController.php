4573484d48fcbdbdbf8e0ecf421c8adb63e5719c
ankita <ankitamakker7@gmail.com>
2017-03-02 22:00:19 +0530

    verify_in_progress

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -84,47 +84,7 @@ class RegisterController extends Controller
 
     // 2/3/17
 
-    public function store()
-    {
-        $rules = [
-           // 'username' => 'required|min:6|unique:users',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|confirmed|min:6'
-        ];
-
-        $input = Input::only(
-           // 'username',
-            'email',
-            'password',
-            'password_confirmation'
-        );
-
-        $validator = Validator::make($input, $rules);
-
-        if($validator->fails())
-        {
-            return Redirect::back()->withInput()->withErrors($validator);
-        }
-
-     //   $confirmation_code = str_random(30);
-
-        // User::create([
-        //     'name' => Input::get('name'), //changed to name
-        //     'email' => Input::get('email'),
-        //     'password' => Hash::make(Input::get('password')),
-        //     'confirmation_code' => $confirmation_code
-        // ]);
-
-
-        // Mail::send('email.verify', $confirmation_code, function($message) {
-        //     $message->to(Input::get('email'), Input::get('name'))
-        //         ->subject('Verify your email address');
-        // });
-
-        // Flash::message('Thanks for signing up! Please check your email.');
-
-        return Redirect::home(); // route register verify confirm code
-    }
+   
 
     public function confirm($confirmation_code)
     {
@@ -197,7 +157,7 @@ class RegisterController extends Controller
         $user->confirmation_code = $confirmation_code;
         $user->save(); // saving in db
 
-         Mail::send('email.verify', $confirmation_code, function($message) {
+         Mail::send('verify', $confirmation_code, function($message) {
             $message->to(Input::get('email'), Input::get('name'))
                 ->subject('Verify your email address');
         });
